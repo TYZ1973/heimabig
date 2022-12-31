31e4998305dd9c93baf371c2168296aae0314fcb
TYZ1973 <zangtianyi@live.com>
2022-12-31 01:33:37 -0400

    1231login

--- assets/js/login.js
+++ assets/js/login.js
@@ -54,7 +54,7 @@ form.verify({
         e.preventDefault()
         
         $.post(
-            'http://ajax.frontend.itheima.net/api/reguser',
+            '/api/reguser',
 
             {
              username: $('#form_reg[name=username]').val(),
@@ -69,6 +69,27 @@ form.verify({
                 layer.msg('注册成功');
 
                 $('#link_login').click();
+            })
+
+            $('#form_login').submit(function(e){
+                e.preventDefault();
+                $.ajax({
+                    url:'/api/login',
+                    method:'POST',
+                    //快速获取表单中的数据
+                    data : $(this).serialize(),
+                    success:function(res){
+                      if(res.status!==0){
+                        return layer.msg('登录失败！！')
+                      }
+                      layer.msg('成功！')
+                      // console.log(res.token);
+                    //将的得到的token 保存到 localstorage
+                      localStorage.setItem('token',res.token)
+                      //跳转到相应的页面
+                      location.href = '/index.html'
+                    }
+                })
             })
         
     })
