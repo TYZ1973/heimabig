586da89eafd5d6f16afd9af33fea7c3efbfcea77
TYZ1973 <zangtianyi@live.com>
2023-01-10 04:22:05 -0400

    0110log

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,4 +2,4 @@ $.ajaxPrefilter(function(options){
     console.log(options.url);
     //options.url 获得的就是 传过来的 /api/reguser  这样的后缀 然后拼接起来
     options.url = 'http://ajax.frontend.itheima.net'+options.url
-})
+})  
